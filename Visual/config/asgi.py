d2e82e090df3b90e8afe9568966b572fe1edbf18
aditya <adityarao1509@gmail.com>
2026-09-22 21:44:57 +0530

    basic changes

--- Visual/config/asgi.py
+++ Visual/config/asgi.py
@@ -1,16 +1,17 @@
-"""
-ASGI config for config project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
-"""
-
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "your_project.settings")
+
 from django.core.asgi import get_asgi_application
+from fastapi_service.main import app as fastapi_app
+from starlette.routing import Mount
+from starlette.applications import Starlette
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+django_app = get_asgi_application()
 
-application = get_asgi_application()
+application = Starlette(
+    routes=[
+        Mount("/process", app=fastapi_app),
+        Mount("/", app=django_app),
+    ]
+)
