4f70fe4be64f99c189acc30ca167b28ab9808e99
aditya <adityarao1509@gmail.com>
2026-09-22 23:44:53 +0530

    minor changes

--- Visual/config/settings.py
+++ Visual/config/settings.py
@@ -67,7 +67,7 @@ SESSION_ENGINE = "django.contrib.sessions.backends.file"
 # Django calls this URL whenever it needs to process data.
 FASTAPI_BASE_URL = "https://dataflow-ggj6.onrender.com"
 FASTAPI_BASE_URL = os.environ.get(
-    "FASTAPI_BASE_URL",
+    FASTAPI_BASE_URL,
     "http://127.0.0.1:8001",
 )
 
