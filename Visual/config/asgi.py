feb11b781d38cb564b89a0444dc5dde2e140fb05
aditya <adityarao1509@gmail.com>
2026-09-22 21:51:52 +0530

    basic changes

--- Visual/config/asgi.py
+++ Visual/config/asgi.py
@@ -1,17 +1,19 @@
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "your_project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 from django.core.asgi import get_asgi_application
-from fastapi_service.main import app as fastapi_app
-from starlette.routing import Mount
 from starlette.applications import Starlette
+from starlette.routing import Mount
+
+from fastapi_service.main import app as fastapi_app
+
 
 django_app = get_asgi_application()
 
 application = Starlette(
     routes=[
-        Mount("/process", app=fastapi_app),
+        Mount("/api", app=fastapi_app),
         Mount("/", app=django_app),
     ]
 )
